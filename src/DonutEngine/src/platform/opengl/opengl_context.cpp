e6d2345ee6eedcfcc69ef611c477c731a49dd084
Hiko <y7513414@Gmail.com>
2023-05-19 22:12:14 +0800

    1. 打印硬件信息

--- src/DonutEngine/src/platform/opengl/opengl_context.cpp
+++ src/DonutEngine/src/platform/opengl/opengl_context.cpp
@@ -3,6 +3,10 @@
 
 #include <glfw/glfw3.h>
 #include <glad/glad.h>
+
+#include <iostream>
+#include <sstream>
+
 namespace Donut
 {
 	OpenGLContext::OpenGLContext(GLFWwindow* win_handle)
@@ -16,6 +20,20 @@ namespace Donut
 		glfwMakeContextCurrent(win_handle_);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		DN_CORE_ASSERT(status, "failed to initialize glad");
+
+		DN_CORE_INFO("OpenGL Info : ");
+
+		std::stringstream ss;
+		ss << glGetString(GL_VENDOR);
+		DN_CORE_INFO("Vendor	: {0}", ss.str());
+
+		ss.clear();
+		ss << glGetString(GL_RENDERER);
+		DN_CORE_INFO("Renderer	: {0}", ss.str());
+
+		ss.clear();
+		ss << glGetString(GL_VERSION);
+		DN_CORE_INFO("Version	: {0}", ss.str());
 	}
 
 	void OpenGLContext::swapBuffers()
